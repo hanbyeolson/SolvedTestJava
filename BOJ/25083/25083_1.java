9f6bd25754c3d1969e28538f12b15b489844c574
hanbyeol <93243019+hanbyeolson@users.noreply.github.com>
2022-08-23 18:48:37 +0900

    BOJ-25083 '새싹' [ 22/08/23 18:48:37 ] by Alub

---
+++ BOJ/25083/25083_1.java
@@ -0,0 +1,17 @@
+/**
+* 메모리: 14012 KB, 시간: 116 ms
+* 2022.08.23
+* by Alub
+*/
+public class Main {
+	public static void main(String[] args) {
+		String s = "         ,r'\"7\n" + 	
+				   "r`-_   ,'  ,/\n" + 		
+				   " \\. \". L_r'\n" + 		
+				   "   `~\\/\n" + 			
+				   "      |\n" + 			
+				   "      |";
+ 
+		System.out.print(s);
+	}
+}
