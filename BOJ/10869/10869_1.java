5284edbe90438c27bd421dd15e15310821cdfaab
hanbyeol <93243019+hanbyeolson@users.noreply.github.com>
2022-08-24 19:40:10 +0900

    BOJ-10869 '사칙연산' [ 22/08/24 19:40:10 ] by Alub

---
+++ BOJ/10869/10869_1.java
@@ -0,0 +1,22 @@
+/**
+* 메모리: 17696 KB, 시간: 216 ms
+* 2022.08.24
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+     public static void main(String[] args) {
+
+         Scanner  sc = new Scanner(System.in);
+
+         int A = sc.nextInt();
+         int B = sc.nextInt();
+
+         System.out.println(A+B);
+         System.out.println(A-B);
+         System.out.println(A*B);
+         System.out.println(A/B);
+         System.out.println(A%B);
+     }
+}
