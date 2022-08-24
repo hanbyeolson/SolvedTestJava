46bb28a7f8fdbe8af7927c34babd5ee68944d17c
hanbyeol <93243019+hanbyeolson@users.noreply.github.com>
2022-08-24 19:44:24 +0900

    BOJ-11382 '꼬마 정민' [ 22/08/24 19:44:23 ] by Alub

---
+++ BOJ/11382/11382_1.java
@@ -0,0 +1,19 @@
+/**
+* 메모리: 17644 KB, 시간: 204 ms
+* 2022.08.24
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+     public static void main(String[] args) {
+
+         Scanner  sc = new Scanner(System.in);
+
+         long A = sc.nextLong();
+         long B = sc.nextLong();
+         long C = sc.nextLong();
+
+         System.out.println(A+B+C);
+     }
+}
