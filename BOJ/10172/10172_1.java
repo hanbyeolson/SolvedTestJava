8cef4e20608f6035d0d3c8ef0acfa9dc71e7eed1
hanbyeol <93243019+hanbyeolson@users.noreply.github.com>
2022-08-24 18:57:20 +0900

    BOJ-10172 '개' [ 22/08/24 18:57:19 ] by Alub

---
+++ BOJ/10172/10172_1.java
@@ -0,0 +1,15 @@
+/**
+* 메모리: 14012 KB, 시간: 116 ms
+* 2022.08.24
+* by Alub
+*/
+public class Main {
+     public static void main(String[] args) {
+        System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");     
+        
+     }
+}
