aacf711abcea7d07f6dc95d02a54eae2b5aae358
hanbyeol <93243019+hanbyeolson@users.noreply.github.com>
2022-08-24 19:36:00 +0900

    BOJ-1008 'A/B' [ 22/08/24 19:35:59 ] by Alub

---
+++ BOJ/1008/1008_1.java
@@ -0,0 +1,18 @@
+/**
+* 메모리: 17732 KB, 시간: 212 ms
+* 2022.08.24
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+     public static void main(String[] args) {
+
+         Scanner  sc = new Scanner(System.in);
+
+         int A = sc.nextInt();
+         int B = sc.nextInt();
+
+         System.out.println((double)A/B);
+     }
+}
