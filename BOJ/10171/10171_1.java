48fa1f74afcafefb67279fcf914a3e9c325d668c
hanbyeol <93243019+hanbyeolson@users.noreply.github.com>
2022-08-24 19:15:42 +0900

    BOJ-10171 '고양이' [ 22/08/24 19:15:42 ] by Alub

---
+++ BOJ/10171/10171_1.java
@@ -0,0 +1,14 @@
+/**
+* 메모리: 13892 KB, 시간: 120 ms
+* 2022.08.24
+* by Alub
+*/
+public class Main {
+     public static void main(String[] args) {
+
+         System.out.println("\\    /\\");
+         System.out.println(" )  ( ')");
+         System.out.println("(  /  )");
+         System.out.println(" \\(__)|");
+     }
+}
