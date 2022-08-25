d23c86c3ef1a157175cbed32095262cf9daa7b5c
hanbyeol <93243019+hanbyeolson@users.noreply.github.com>
2022-08-25 18:37:58 +0900

    BOJ-1330 '두 수 비교하기' [ 22/08/25 18:37:58 ] by Alub

---
+++ BOJ/1330/1330_1.java
@@ -0,0 +1,26 @@
+/**
+* 메모리: 17752 KB, 시간: 204 ms
+* 2022.08.25
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+     public static void main(String[] args) {
+
+         Scanner  sc = new Scanner(System.in);
+
+         int A = sc.nextInt();
+         int B = sc.nextInt();
+
+         if (A < B) {
+             System.out.println("<");
+         } else if (A > B) {
+             System.out.println(">");
+         } else {
+             System.out.println("==");
+         }
+
+         return;
+     }
+}
