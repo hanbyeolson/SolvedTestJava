f33e191e34fcc83b9460da0dea60781c461b0370
hanbyeol <93243019+hanbyeolson@users.noreply.github.com>
2022-08-27 15:40:50 +0900

    BOJ-2420 '사파리월드' [ 22/08/27 15:40:50 ] by Alub

---
+++ BOJ/2420/2420_1.java
@@ -0,0 +1,16 @@
+/**
+* 메모리: 17624 KB, 시간: 208 ms
+* 2022.08.27
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+     public static void main(String[] args) {
+
+         Scanner sc = new Scanner(System.in);
+         
+         long n = sc.nextInt(), m = sc.nextInt();
+         System.out.println(Math.abs(n-m));
+     }
+}
