2c05de7f46f6df458ec2c1a5c2983b3ebe5b19c2
hanbyeol <93243019+hanbyeolson@users.noreply.github.com>
2022-08-31 18:26:22 +0900

    BOJ-2741 'N 찍기' [ 22/08/31 18:26:22 ] by Alub

---
+++ BOJ/2741/2741_1.java
@@ -0,0 +1,18 @@
+/**
+* 메모리: 33104 KB, 시간: 1028 ms
+* 2022.08.31
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+
+        for (int i = 1; i < N + 1; i++) {
+            System.out.println(i);
+        }
+    }
+}
