51f3573757f7d2b07d30e96b4a940c1e02f24d7f
hanbyeol <93243019+hanbyeolson@users.noreply.github.com>
2022-09-03 01:29:24 +0900

    BOJ-10872 '팩토리얼' [ 22/09/03 01:29:24 ] by Alub

---
+++ BOJ/10872/10872_1.java
@@ -0,0 +1,25 @@
+/**
+* 메모리: 17592 KB, 시간: 208 ms
+* 2022.09.03
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        sc.close();
+
+        int sum = factorial(N);
+        System.out.println(sum);
+
+    }
+
+    public static int factorial(int N){
+        if (N <= 1) return 1;
+        return N * factorial(N - 1);
+    }
+}
